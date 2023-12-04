8212a0e5123c7152a871f6ab5aec3b49d20af124
negralessio <Alessio.Negrini@live.de>
2023-12-04 18:26:25 +0100

    Implemented first outline of main.py

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+import src.utils as utils
+
+from src.dataloading import DataLoader
+from src.preprocessing import Preprocesser
+
+
 def main():
-    pass
+    cfg, config_path = utils.parse_config_file()
+
+    # Load data as dataframes
+    dataloader = DataLoader(**cfg["dataloading"])
+    df_list = dataloader.load_data()
+
+    # Apply preprocessing pipeline to each df in df_list
+    preprocesser = Preprocesser(df_list=df_list, **cfg["preprocessing"])
+    df_list = preprocesser.preprocess_data()
+
 
 
 if __name__ == '__main__':
