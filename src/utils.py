370a067a28f6599b8e247fdd5a782f0addddd31e
negralessio <Alessio.Negrini@live.de>
2023-12-04 18:03:01 +0100

    Implemented argparsing and config yaml handling

---
+++ src/utils.py
@@ -0,0 +1,37 @@
+import yaml
+import argparse
+
+
+def load_config(config_path: str) -> dict:
+    """
+    Loads a YAML configuration file as a dict.
+
+    :param config_path: str -- Path to the configuration file
+    :return: dict -- Configuration dictionary
+    """
+    try:
+        with open(config_path, "r") as yamlfile:
+            return yaml.load(yamlfile, yaml.FullLoader)
+    except FileNotFoundError:
+        raise FileNotFoundError(f"File {config_path} not found!")
+    except PermissionError:
+        raise PermissionError(f"Insufficient permission to read {config_path}!")
+    except IsADirectoryError:
+        raise IsADirectoryError(f"{config_path} is a directory!")
+
+
+def parse_config_file() -> tuple[dict, str]:
+    """
+    Function that parses the config.yaml
+
+    :return:    cfg: dict -- Parsed config file as dict
+                args.config: str -- Config path
+    """
+    # Parse config file
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config", help="Path to the config file", type=str, default="configs/config.yaml")
+    args = parser.parse_args()
+
+    # Load config file
+    cfg = load_config(args.config)
+    return cfg, args.config
