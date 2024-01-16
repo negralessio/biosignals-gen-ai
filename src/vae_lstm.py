64116ec6fd21f9c25950694b7f1f5410b2d9f1c4
negralessio <Alessio.Negrini@live.de>
2024-01-16 18:34:15 +0100

    Made model more complex

--- src/vae_lstm.py
+++ src/vae_lstm.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 
-from tensorflow.keras.layers import Input, LSTM, Dense, TimeDistributed, Reshape, Dropout
+from tensorflow.keras.layers import Input, LSTM, Dense, TimeDistributed, Reshape, Dropout, Bidirectional, BatchNormalization
 
 from src.vae_base import BaseVAE, Sampling
 
@@ -15,12 +15,13 @@ class LSTMVAE(BaseVAE):
         """ Defines and returns Encoder architecture """
 
         inputs = Input(shape=(self.sequence_length, self.num_features))
-        z = LSTM(64)(inputs)
-        z_mean = Dense(self.latent_dims, name="z_mean")(z)
-        z_log_var = Dense(self.latent_dims, name="z_log_var")(z)
+        x = Bidirectional(LSTM(128, return_sequences=True))(inputs)
+        x = Bidirectional(LSTM(128))(x)
+        x = BatchNormalization()(x)
+        z_mean = Dense(self.latent_dims, name="z_mean")(x)
+        z_log_var = Dense(self.latent_dims, name="z_log_var")(x)
         z = Sampling()([z_mean, z_log_var])
         encoder = tf.keras.Model(inputs=[inputs], outputs=[z_mean, z_log_var, z], name="encoder")
-
         return encoder
 
     def get_decoder(self):
@@ -29,10 +30,12 @@ class LSTMVAE(BaseVAE):
         decoder_inputs = Input(shape=(self.latent_dims,))
         x = Dense(self.sequence_length * 1, activation="relu", name='Decode_1')(decoder_inputs)
         x = Reshape((self.sequence_length, 1), name='Decode_2')(x)
-        x = LSTM(50, return_sequences=True)(x)
+        x = Bidirectional(LSTM(128, return_sequences=True))(x)
+        x = BatchNormalization()(x)
+        x = Bidirectional(LSTM(128, return_sequences=True))(x)
+        x = BatchNormalization()(x)
         decoder_output = TimeDistributed(Dense(self.num_features, activation='linear'), name='Decoder_Output_Layer')(x)
         decoder = tf.keras.Model(inputs=decoder_inputs, outputs=decoder_output, name="decoder")
-
         return decoder
 
 
