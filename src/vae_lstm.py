4261a48a1d67c22dd9a63421e1d5cdb446a3fa53
negralessio <Alessio.Negrini@live.de>
2024-01-18 18:23:19 +0100

    Changed architecture of LSTM Encoder/Decoder

--- src/vae_lstm.py
+++ src/vae_lstm.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 
-from tensorflow.keras.layers import Input, LSTM, Dense, TimeDistributed, Reshape, Dropout, Bidirectional, BatchNormalization
+from tensorflow.keras.layers import Input, LSTM, Dense, TimeDistributed,\
+    Reshape, Bidirectional, BatchNormalization, RepeatVector
 
 from src.vae_base import BaseVAE, Sampling
 
@@ -15,12 +16,13 @@ class LSTMVAE(BaseVAE):
         """ Defines and returns Encoder architecture """
 
         inputs = Input(shape=(self.sequence_length, self.num_features))
-        x = Bidirectional(LSTM(128, return_sequences=True))(inputs)
-        x = Bidirectional(LSTM(128))(x)
+        x = Bidirectional(LSTM(32, return_sequences=True))(inputs)
+        x = Bidirectional(LSTM(32))(x)
         x = BatchNormalization()(x)
         z_mean = Dense(self.latent_dims, name="z_mean")(x)
         z_log_var = Dense(self.latent_dims, name="z_log_var")(x)
         z = Sampling()([z_mean, z_log_var])
+
         encoder = tf.keras.Model(inputs=[inputs], outputs=[z_mean, z_log_var, z], name="encoder")
         return encoder
 
@@ -28,14 +30,12 @@ class LSTMVAE(BaseVAE):
         """ Defines and returns Decoder architecture """
 
         decoder_inputs = Input(shape=(self.latent_dims,))
-        x = Dense(self.sequence_length * 1, activation="relu", name='Decode_1')(decoder_inputs)
-        x = Reshape((self.sequence_length, 1), name='Decode_2')(x)
-        x = Bidirectional(LSTM(128, return_sequences=True))(x)
+        x = RepeatVector(self.sequence_length)(decoder_inputs)
+        x = Bidirectional(LSTM(32, return_sequences=True))(x)
         x = BatchNormalization()(x)
-        x = Bidirectional(LSTM(128, return_sequences=True))(x)
+        x = Bidirectional(LSTM(32, return_sequences=True))(x)
         x = BatchNormalization()(x)
         decoder_output = TimeDistributed(Dense(self.num_features, activation='linear'), name='Decoder_Output_Layer')(x)
+
         decoder = tf.keras.Model(inputs=decoder_inputs, outputs=decoder_output, name="decoder")
         return decoder
-
-
